788fe1c3650343389ebe55f42f5e6e3ea4ef0799
Shelly Yusuf <ShellyNYusuf@gmail.com>
2018-07-07 23:13:57 -0700

    adding files

---
+++ public/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Next from './components/Next';
+// import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+
+const jsx =(
+  <div>
+  <Next />
+  </div>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
